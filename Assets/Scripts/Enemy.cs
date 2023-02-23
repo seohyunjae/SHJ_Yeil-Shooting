79014b8f73da07deafe4a6af3c7e3a5ab8fcc762
seohyunjae <97527644+seohyunjae@users.noreply.github.com>
2023-02-23 11:50:27 +0900

    Initial commit

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,150 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed;
+    public float health;
+    public int enemyscore;
+
+
+    public GameObject cherry;
+    public GameObject starryberry;
+    public GameObject watermelon;
+
+    public Sprite[] sprites;
+    SpriteRenderer spriteRender;
+
+    Rigidbody2D rd;
+
+    public GameObject bulletPrefab;
+    public float curBulletDelay = 0f;
+    public float maxBulletDelay = 1f;
+
+    public GameObject playerObject;
+    
+    bool isDead = false;
+
+
+    void Awake()
+    {
+        rd = GetComponent<Rigidbody2D>();
+        //rd.velocity = Vector2.down * speed; => Move 함수로 이동
+        spriteRender = GetComponent<SpriteRenderer>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        //rd = GetComponent<Rigidbody2D>();
+        ////rd.velocity = Vector2.down * speed; => Move 함수로 이동
+        //spriteRender = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Fire();
+        ReloadBullet();
+    }
+
+    void Fire()
+    {
+        if (curBulletDelay > maxBulletDelay)
+        {
+            Power();
+
+            curBulletDelay = 0;
+        }
+    }
+
+    void Power()
+    {
+        GameObject bulletObj = Instantiate(bulletPrefab, transform.position, Quaternion.identity);
+        Rigidbody2D rdBullet = bulletObj.GetComponent<Rigidbody2D>();
+        
+        Vector3 dirVec = playerObject.transform.position - transform.position;
+        rdBullet.AddForce(dirVec.normalized * 3, ForceMode2D.Impulse);
+    }
+
+    void ReloadBullet()
+    {
+        curBulletDelay += Time.deltaTime;
+    }
+
+    public void Move(int nPoint)
+    {
+        if (nPoint == 3 || nPoint == 4) // 오른쪽에 있는 스폰 포인트의 배열 인덳스값
+        {
+            transform.Rotate(Vector3.back * 90);
+            rd.velocity = new Vector2(speed * (-1), -1);
+        }
+        else if (nPoint == 5 || nPoint == 6) // 왼쪽에 있는 스폰 포인트의 배열 인덳스값
+        {
+            transform.Rotate(Vector3.forward * 90);
+            rd.velocity = new Vector2(speed, -1);
+        }
+        else
+        {
+            rd.velocity = Vector2.down * speed;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Border")
+        {
+            Destroy(gameObject);
+        }
+        else if (collision.gameObject.tag == "PlayerBullet")
+        {
+            Bullet bullet = collision.gameObject.GetComponent<Bullet>();
+
+            OnHit(bullet.power);
+            Destroy(collision.gameObject);
+           
+        }
+    }
+
+    private void OnHit(float BulletPower)
+    {
+        health -= BulletPower;
+        spriteRender.sprite = sprites[1];
+        Invoke("ReturnSprite", 0.1f);
+
+        if (health <= 0 && isDead == false)
+        {
+
+            isDead = true;
+            Destroy(gameObject);
+            Player playerscore = playerObject.GetComponent<Player>();
+            playerscore.score += 1;
+
+            //random
+            int random = Random.Range(0, 10);
+            if (random < 5) {
+                Debug.Log("Not item");
+            }
+            else if (random < 8)
+            {
+                Instantiate(cherry, transform.position, cherry.transform.rotation);
+            }
+            else if(random <9)
+            {
+                Instantiate(starryberry, transform.position, cherry.transform.rotation);
+            }
+            else if(random <10)
+            {
+                Instantiate(watermelon, transform.position, cherry.transform.rotation);
+            }
+
+        }
+    }
+
+    void ReturnSprite()
+    {
+        spriteRender.sprite = sprites[0];
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,125 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    public Transform[] spawnPoints;
+    public GameObject[] enemyPrefabs;
+
+    public float curEnemySpawnDelay;
+    public float nextEnemySpawnDelay;
+    public GameObject gameoverText;
+    public Text timeText; //생존시간
+    public Text scoreText; //점수텍스트
+    public Text LifeText; //목숨
+
+
+    private float suriveTime; //생존 시간
+    private bool isGameover; //게임오버 상태
+    public GameObject player;
+    public bool isRespawnTime; //bool 타입 변수 선언
+    private bool attackSucess; //어택성공
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //생존 시간 게임오버 초기화
+        suriveTime = 0;
+        isGameover = false;
+    }
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }        
+        else
+        {
+            Destroy(gameObject);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        curEnemySpawnDelay += Time.deltaTime;
+        if (curEnemySpawnDelay > nextEnemySpawnDelay)
+        {
+            SpawnEnemy();
+
+            nextEnemySpawnDelay = Random.Range(0.5f, 3.0f);
+            curEnemySpawnDelay = 0;
+
+
+        }
+        if (!isGameover)
+        {
+            suriveTime += Time.deltaTime;
+            timeText.text = "SURVIVAL TIME:" + (int)suriveTime;
+
+            Player playerscore = player.GetComponent<Player>();
+
+            
+            scoreText.text = "SCORE:" + (int)playerscore.score;
+
+            Player lifeText = player.GetComponent<Player>();
+
+            LifeText.text = "LIFE:" + (int)lifeText.life;
+
+
+            
+        }
+        
+
+    }
+
+    void SpawnEnemy()
+    {
+
+        int ranType = Random.Range(0, enemyPrefabs.Length);
+        int ranPoint = Random.Range(0, spawnPoints.Length);
+        GameObject goEnemy = Instantiate(enemyPrefabs[ranType], spawnPoints[ranPoint].position, Quaternion.identity);
+        Enemy enemyLogic = goEnemy.GetComponent<Enemy>();
+        enemyLogic.playerObject = player;
+        enemyLogic.Move(ranPoint);
+    }
+
+    public void GameOver()
+    {
+        
+    }
+
+    public void RespawnPlayer()
+    {
+        Invoke("AlivePlayer", 1.0f);
+        
+        
+    }
+
+    void AlivePlayer()
+    {
+        player.transform.position = Vector3.down * 4.2f;
+        player.SetActive(true);
+        player.GetComponent<PolygonCollider2D>().enabled = false;
+        
+        Player playrLogic = player.GetComponent<Player>();
+        playrLogic.isHit = false;
+        Invoke(nameof(Delayplayer), 2f); 
+        
+        
+
+
+    }
+    void Delayplayer()
+    {
+        player.GetComponent<PolygonCollider2D>().enabled = true;
+    }
+   
+}
